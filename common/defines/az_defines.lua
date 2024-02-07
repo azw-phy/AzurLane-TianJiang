1bb49e235681ac00f0e8aa1e2ad6aac3b27da092
azw <azwphy@foxmail.com>
2024-02-08 04:42:03 +0800

    0208base

--- common/defines/az_defines.lua
+++ common/defines/az_defines.lua
@@ -23,6 +23,14 @@ NDefines.NCountry.BASE_MAX_COMMAND_POWER = 400
 NDefines.NCountry.BASE_COMMAND_POWER_GAIN = 0.6	
 NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 35
 --下划线
+NDefines.NDiplomacy.MAX_TRUST_VALUE = 200						-- 最大关系上限
+NDefines.NDiplomacy.MIN_TRUST_VALUE = -200						-- 最小关系上限
+NDefines.NDiplomacy.MAX_OPINION_VALUE = 200						-- 最大关系上限
+NDefines.NDiplomacy.MIN_OPINION_VALUE = -200						-- 最小关系上限
+NDefines.NCountry.WAR_SUPPORT_OFFNSIVE_WAR = -0.3				-- 进攻战争影响的战争支持度
+NDefines.NCountry.WAR_SUPPORT_DEFENSIVE_WAR = 0.3			-- 防御战争影响的战争支持度
+NDefines.NCountry.WAR_SUPPORT_TENSION_IMPACT = 0.5			-- 世界紧张度影响的战争支持度
+NDefines.NProduction.MINIMUM_NUMBER_OF_FACTORIES_TAKEN_BY_CONSUMER_GOODS_PERCENT = 0.05		-- 最低消费品系数
 NDefines.NDiplomacy.PEACE_SCORE_SCALE_FACTOR = 2.25							--战胜国从战败国那里获得的战争分倍数
 NDefines.NDiplomacy.PEACE_SCORE_DISTRIBUTION = { 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2 }		--可获得分数的和谈轮数
 NDefines.NAI.ASSIGN_TANKS_TO_WAR_FRONT = 10								--将坦克师分配到前线的得分因素
